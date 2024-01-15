e95e063bf1e095c9ecabede2e2b6f2582e0249ba
Paulo Henrique Aleixo de Campos <119341826+phaleixo@users.noreply.github.com>
2024-01-14 22:28:16 -0300

    Trocar Radeon para Amdgpu
    Script para ativar o drive amdgpu e desativar o drive Radeon no linux, testada no Debian 12, mas deve funcionar em qualquer distro baseada em Debian.
    Abaixo está uma lista de todos os modelos de GPUs das famílias ‘Sea Islands’ e ‘Southern Islands’.
    Caso a sua GPU esteja nessa lista, então o procedimento provavelmente será necessário.  
    
    Tahiti,
    Bonaire,
    Hawaii, 
    Pitcairn,
    Cape Verde,
    Oland.

---
+++ radeon_to_amdgpu.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------------------------------- #
+# ---------------------------------------- CABEÇALHO ------------------------------------------ #
+## AUTOR:
+### 	Paulo Aleixo <contato.phaleixo@outlook.com.br>
+## NOME:
+###     Trocar Radeon para Amdgpu
+
+
+drive_video_card=$(lspci -k | grep amdgpu)
+video_card=$(lspci | grep VGA)
+
+if [[ "$drive_video_card" = *"Kernel driver in use: amdgpu"* ]]; then
+   
+   echo " Placa de video:"
+   echo " '$video_card'"
+   echo "--------------------------------------------------------------------"
+   echo " O driver amdgpu já está ativo"
+elif [[ "$drive_video_card" = *"Kernel driver in use: radeon"* ]]; then
+   echo "--------------------------------------------------------------------"
+   echo " Placa de video:"
+   echo " '$video_card'"
+   echo "--------------------------------------------------------------------"
+   echo "Alterando drive radeon para amdgpu"
+   sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 radeon.cik_support=0 amdgpu.cik_support=1 radeon.si_support=0 amdgpu.si_support=1"/' /etc/default/grub
+   sudo update-grub
+   echo "--------------------------------------------------------------------"
+   echo "Configuração do driver atualizada. Reinicie o sistema para aplicar as alterações."
+   echo "--------------------------------------------------------------------"
+else
+   echo "--------------------------------------------------------------------"
+   echo "Placa de video:"
+   echo "'$video_card'"
+   echo "Não foi possível alterar o driver de vídeo atual."
+   
+fi
+
